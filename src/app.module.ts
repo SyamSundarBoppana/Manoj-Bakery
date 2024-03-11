8642f8c58a92c7d83428023e3c535a13e7775519
syam-flyerssoft <syam.boppana@flyerssoft.com>
2024-03-11 17:36:43 +0530

    file upload added and data saved to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { EmployeeModule } from './employee/employee.module';
     TypeOrmModule.forRoot({
       name: "default",
       type: 'postgres',
-      url:"postgresql://postgres:zaUeFpsNwxztGBlHFgmeVbWeXPsfQrAl@viaduct.proxy.rlwy.net:27040/railway",
+      url:"postgres://fmvdajow:q4iwQxU5xQWqpNpuxq6Mb8NibkNThP0u@ziggy.db.elephantsql.com/fmvdajow",
       entities: [Employee],
       synchronize: true,
       logging: true,
--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,16 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { Employee } from './entities/employee.entity';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { read, utils } from 'xlsx';
+
 
 @Controller('employee')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto):Promise<Employee> {
+  create(@Body() createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
     return this.employeeService.create(createEmployeeDto);
+  }
+
+  @Post('upload')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    type: 'multipart/form-data',
+    required: true,
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile('file') file: Express.Multer.File) {
+    const workBook = read(file.buffer);
+    const sheetName = workBook.SheetNames[0];
+    const workSheet = workBook.Sheets[sheetName];
+    const jsonData = utils.sheet_to_json(workSheet,{header:0});
+    jsonData.map((data:CreateEmployeeDto)=>{
+      this.employeeService.create(data)
+    })
+    return 'File Uploaded Sucessfully'
   }
 
   @Get()
@@ -24,7 +65,10 @@ export class EmployeeController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateEmployeeDto: UpdateEmployeeDto,
+  ) {
     return this.employeeService.update(+id, updateEmployeeDto);
   }
 
